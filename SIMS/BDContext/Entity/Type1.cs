5f79d1e7452527094d1007e84b374e01cf779358
Tuannguyen1312 <anhtuan130412@gmail.com>
2025-08-09 03:57:08 +0700

    Crud

---
+++ SIMS/BDContext/Entity/Type1.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SIMS.BDContext.Entity
+{
+    public class Type1
+    {
+        [Key]
+        public int TypeID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? TypeName { get; set; }
+
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Description { get; set; }
+
+        [AllowNull]
+        public DateTime? CreatedAt { get; set; }
+
+        [AllowNull]
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+}
